36c377aa321756cd2282bbd3e2a7bd0a64bddc02
Joe Henriod <joehenriod@gmail.com>
2015-08-06 23:51:52 +0000

    NavBar GliphCon

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
-gem 'bootstrap-sass', '~> 3.3.5'
+gem 'bootstrap-sass', '3.3.4.1'
+
+gem 'sprockets', '<= 2.11.0'
 
 gem "will_paginate", "~> 3.0.4" 
 
@@ -13,7 +15,7 @@ gem 'rails', '4.0.1'
 gem 'pg'
 
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.0'
+gem 'sass-rails'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,21 @@
 class PlacesController < ApplicationController
-    def index
+  def index
         @places = Place.paginate(:page => params[:page], :per_page => 5)
     end
+  def new
+    @place = Place.new
+  end
 
-    def new
-    	  @place = Place.new
-    end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
