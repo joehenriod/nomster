9f23e3dff431672980ea588cf519e2fe01fc15bf
Joe Henriod <joehenriod@gmail.com>
2015-08-08 22:17:52 +0000

    User Validation Errors

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,15 +3,21 @@ class PlacesController < ApplicationController
   def index
         @places = Place.paginate(:page => params[:page], :per_page => 5)
     end
+
   def new
     @place = Place.new
   end
 
 
-  def create
-  	current_user.places.create(place_params)
+ def create
+  @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, :status => :unprocessable_entity
   end
+end
+
 
   def show
     @place = Place.find(params[:id])
@@ -26,15 +32,20 @@ class PlacesController < ApplicationController
   end
 
   def update
-    @place = Place.find(params[:id])
+  @place = Place.find(params[:id])
 
-    if @place.user != current_user
+  if @place.user != current_user
     return render :text => 'Not Allowed', :status => :forbidden
   end
 
-    @place.update_attributes(place_params)
+  @place.update_attributes(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :edit, :status => :unprocessable_entity
   end
+end
+
 
   def destroy
     @place = Place.find(params[:id])
