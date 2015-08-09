d1b704b216e507a202525eb123e4656116e82369
Joe Henriod <joehenriod@gmail.com>
2015-08-09 03:55:45 +0000

    Added Bing Map Keys

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,10 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+
+	geocoded_by :address
+  	after_validation :geocode
+
 	validates :name, :description, :address, :presence => true
-	validates :name, length: { minimum: 10 }
+	validates :name, length: { minimum: 5 }
+
 end
