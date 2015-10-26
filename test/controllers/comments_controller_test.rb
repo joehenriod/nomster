e0d8edc2aa470c05be9f8d06e155e0c5725a80cf
Joe Henriod <joehenriod@gmail.com>
2015-10-26 00:59:42 +0000

    Comments Controller Test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create_comments" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+
+    place = FactoryGirl.create(:place)
+    post :create, :place_id => place.id, :comment => {
+      :message => 'Factory Message',
+      :rating => '1_star',
+      :user_id => user.id
+    }
+    assert_not_empty(place.comments)
+    assert_redirected_to place_path(place)
+  end
 end
