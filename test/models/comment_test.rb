5ffb08d7ba66d59c2dfb4b6302531dff43cc4e1b
Joe Henriod <joehenriod@gmail.com>
2015-10-26 05:52:30 +0000

    uptodate

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,4 +4,11 @@ class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "comment humanized" do
+    quote = Comment.new(:rating => '1_star').humanized_rating 
+    get :show, :id => quote.id
+    assert_response :success
+
+    
 end
