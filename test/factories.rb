1f88850731709c6d79eacb35cbe86ce40189c05c
Joe Henriod <joehenriod@gmail.com>
2015-10-27 20:24:05 +0000

    up2date

--- test/factories.rb
+++ test/factories.rb
@@ -3,4 +3,20 @@ FactoryGirl.define do
     author "Gary Vaynerchuk"
     saying "I love people, and the hustle."
   end
+
+  factory :user do
+    sequence(:email) { |n| "person#{n}@example.com"}
+      password "87654321"
+      password_confirmation "87654321"
+  end
+
+  factory :place do
+    name "Ye Ol' Tattoo Shop"
+    address "101 N. Las Vegas Blvd."
+    description "Check out my Dragons Bro!"
+    latitude (36.170048)
+    longitude (115.139744)
+    association :user
+  end
+
 end
